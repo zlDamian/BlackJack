4aabf271e3402a392fb1239c7d8627affc39ef6a
DaMian <DaMian@PC1-D>
2024-04-22 01:14:46 +0200

    BlackJack Anfang

--- src/Blackjack.java
+++ src/Blackjack.java
@@ -1,10 +1,32 @@
+import java.util.Scanner;
+import java.util.InputMismatchException;
 
 public class Blackjack {
 
 	public static void main(String[] args) {
-		System.out.println("Test");
-		System.out.println("Test");
 		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Willkommen zu unserem!");
+		System.out.println(
+	                "\n――――――――――――――――――――――――――\n  " +
+	                "♥♦ Black Jack ♣♠" +
+	                "\n――――――――――――――――――――――――――\n");
+		System.out.print("Gib deinen Namen ein: ");
+		String player = new String(sc.nextLine());
+		
+		System.out.println();
+		System.out.println("Willkommen " + player + "! Dein aktuelles Guthaben beträgt: €\n");
+		System.out.println("Los gehts...");
+		System.out.println();	
+	                
+	                
+	                
+	                
+	                
+	                
+	                
+	                
 	}
 
 }
