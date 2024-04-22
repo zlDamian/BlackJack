f47b1eb487e345426be17ae0df9418b85914d81f
DaMian <DaMian@PC1-D>
2024-04-22 10:30:52 +0200

    Einsatz setzen

--- src/Blackjack.java
+++ src/Blackjack.java
@@ -1,10 +1,13 @@
 import java.util.Scanner;
+import java.util.Random;
 
 public class Blackjack {
 
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
+		boolean nochmal = true;
+		int guthaben = 100;
 		
 		System.out.println("Willkommen zu unserem!");
 		System.out.println(
@@ -15,9 +18,29 @@ public class Blackjack {
 		String player = new String(sc.nextLine());
 		
 		System.out.println();
-		System.out.println("Willkommen " + player + "! Dein aktuelles Guthaben beträgt: €\n");
-		System.out.println("Los gehts...");
+		System.out.println("Willkommen " + player + "! Dein aktuelles Guthaben beträgt: €" + guthaben);
+		System.out.println("\nLos gehts...");
 		System.out.println();	
+		
+		
+		while (nochmal == true) {
+			
+			int einsatz = 0;
+			System.out.println("Gib deinen Einsatz ein:");
+			einsatz = sc.nextInt();
+			
+			 while(einsatz < 1 || einsatz > guthaben) {
+				 
+					System.out.println("Dein Einsatz ist ungültig, gib es nochmal ein");
+					einsatz = sc.nextInt();
+			 }
+			
+			
+			
+			
+			Random karte = new Random();
+			
+		}
 	                
 	                
 	                
